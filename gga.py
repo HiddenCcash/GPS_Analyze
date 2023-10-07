415ead3059686318355a7294df5f20454b3a783c
hiddenccash <hiddenccash@gmail.com>
2023-10-07 03:07:06 -0700

    finish

---
+++ gga.py
@@ -0,0 +1,18 @@
+""""    This file passes a file as the first argurment and parses GPGGA sentences, line-by-line    ""
+
+import sys, re
+
+path = sys.argv[1]
+list_line = []
+with open(path,'r') as file:
+    for line in file:
+        if (re.search('GGA',line)):
+            line = line.split("$")
+            line = line[1].split(',')
+            list_line.append(line)
+
+cvs_file = path + ".csv"
+with open(cvs_file, 'w') as cvs_f:
+    writer  = cvs_file
+    for i in range(len(list_line)):
+        writer.writerow( (i+1, list_line[i]) )
